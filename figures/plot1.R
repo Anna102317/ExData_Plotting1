873e2ae5d41c5a2d4e00e807a99c4aff2718ba9f
Anna <67330824+Anna102317@users.noreply.github.com>
2020-07-11 19:00:04 -0400

    Update plot1.R

--- figures/plot1.R
+++ figures/plot1.R
@@ -4,4 +4,12 @@ householdFile <- "/Users/jiaoweiqi/OneDrive - Johns Hopkins University/R/househo
 if (!file.exists(householdFile)) {
         download.file(downloadURL, downloadFile, method = "curl")
         unzip(downloadFile, overwrite = T, exdir = "./Data")
-
+##
+plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
+## set time variable
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+##
+## Generating Plot 1
+hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
